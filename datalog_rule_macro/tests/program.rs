a2ad96c81de9a08e9c6109d114d10e8a336341fa
kasesalum <kasesalum@gmail.com>
2024-06-06 17:19:04 +0300

    Create semipositive program macro

--- datalog_rule_macro/tests/program.rs
+++ datalog_rule_macro/tests/program.rs
@@ -1,17 +1,21 @@
 #[cfg(test)]
 mod tests {
     use datalog_rule_macro::program;
+    use datalog_rule_macro::semipositive_program;
     use datalog_rule_macro::rule;
     use datalog_syntax::*;
     use pretty_assertions::assert_eq;
 
     #[test]
     fn test_program() {
-        let expected_program = Program::from(vec![
-            rule! { tc(?x, ?y) <- [e(?x, ?y)] }, // esimene y peaks tglt z olema?
-            rule! { tc(?x, ?z) <- [e(?x, ?y), tc(?y, ?z)] },
-        ]);
-        let actual_program = program! {
+        let expected_program = Program::from(
+            vec![
+                rule! { tc(?x, ?y) <- [e(?x, ?y)] },
+                rule! { tc(?x, ?z) <- [e(?x, ?y), tc(?y, ?z)] }
+            ]
+        );
+        let actual_program =
+            program! {
             tc(?x, ?y) <- [e(?x, ?y)],
             tc(?x, ?z) <- [e(?x, ?y), tc(?y, ?z)]
         };
@@ -24,6 +28,23 @@ mod tests {
         // Second task =)
         // 1 - Implement semi-positive rule evaluation
         // 2 - Implement stratified evaluation
+
+        assert_eq!(expected_program, actual_program);
+    }
+
+    #[test]
+    fn test_semipositive_program() {
+        let expected_program = Program::from(
+            vec![
+                rule! { tc(?x, ?y) <- [e(?x, ?y)] },
+                rule! { tc(?x, ?z) <- [!e(?x, ?y), tc(?y, ?z)] }
+            ]
+        );
+        let actual_program =
+            semipositive_program! {
+                tc(?x, ?y) <- [e(?x, ?y)], 
+                tc(?x, ?z) <- [!e(?x, ?y), tc(?y, ?z)] 
+        };
 
         assert_eq!(expected_program, actual_program);
     }
